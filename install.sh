9af399e78d99fc23ed56125a953027dbcf93d64e
al4uu <al4auuu@gmail.com>
2024-12-08 13:31:47 +0700

    MODULE: update install.sh v2.2
    - Added a new, more interactive UI installer for better user experience.  
    - Dropped unnecessary dex-opt commands:  
      - `cmd package force-dex-opt com.android.systemui`  
      - `cmd package bg-dexopt-job`  
    - Improved installation speed by reducing redundant operations.  
    - General performance optimizations and bug fixes.

--- install.sh
+++ install.sh
@@ -4,22 +4,10 @@ PROPFILE=true
 POSTFSDATA=true
 LATESTARTSERVICE=true
 
-ui_print "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀"
-ui_print "⢀⢀⢀⢀⢀⢀⢀⡤⠒⠋⠉⠉⠉⠉⠉⠑⠲⢄⢀⢀⢀⢀⢀⢀⢀"
-ui_print "⢀⢀⢀⢀⢀⠔⢁⡠⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠉⢢⢀⢀⢀⢀⢀"
-ui_print "⢀⢀⢀⢠⠃⣴⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠱⡄⢀⢀⢀"
-ui_print "⢀⢀⢠⠃⢀⣿⣧⣀⢀⢀⣀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠹⡀⢀⢀"
-ui_print "⢀⢀⡇⢀⢀⠹⣶⣿⣿⣿⣿⣿⣶⣶⣶⣤⡀⢀⢀⣴⡿⢀⡇⢀⢀"
-ui_print "⢀⢀⡇⢀⢀⢀⠤⣾⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⢀⡇⢀⢀"
-ui_print "⢀⢀⡇⢀⣠⣤⣶⣿⣿⣧⣉⣿⣿⡟⣃⣿⢿⡿⢀⢀⢀⢀⡇⢀⢀"
-ui_print "⢀⢀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⢀⢀⢀⢀⢀⡰⠁⢀⢀"
-ui_print "⢀⢀⢀⠘⣿⣿⣿⣿⣿⣏⡀⢀⣸⣿⣿⢀⢀⢀⢀⢀⡰⠃⢀⢀⢀"
-ui_print "⢀⢀⢀⢀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⡏⢀⢀⢀⣠⠞⠁⢀⢀⢀⢀"
-ui_print "⢀⢀⢀⢀⢀⢀⠈⠙⠿⢿⣿⣿⣿⣿⣇⡠⠔⠊⠁⢀⢀⢀⢀⢀⢀"
-ui_print "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀"
-ui_print "- Bumbu Racik"
+ui_print "█▄▄ █░█ █▀▄▀█ █▄▄ █░█   █▀█ ▄▀█ █▀▀ █ █▄▀"
+ui_print "█▄█ █▄█ █░▀░█ █▄█ █▄█   █▀▄ █▀█ █▄▄ █ █░█"
 ui_print "- Author : @al4uu"
-ui_print "- Version : 2.1"
+ui_print "- Version : 2.2"
 ui_print " "
 ui_print "- Installing.."
 
@@ -28,10 +16,6 @@ sleep 5
 unzip "$ZIPFILE" system/* -d "$MODPATH/" >/dev/null 2>&1
 
 cp -af "$TMPDIR"/common/uninstall.sh "$MODPATH"/uninstall.sh >/dev/null 2>&1
-
-cmd package force-dex-opt com.android.systemui >/dev/null 2>&1
-
-cmd package bg-dexopt-job >/dev/null 2>&1
 
 sleep 5
 
